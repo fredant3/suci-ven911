e34b6e6fd0ac74e7efbe2549bd64eeb4fedc33a0
Erick Perez <erickjpl@gmail.com>
2024-11-23 19:29:14 -0400

    migraciones y lenvantando en el navegador

---
+++ apps/seguridad/models.py
@@ -0,0 +1,6 @@
+from seguridad.entradas.models import Entrada
+from seguridad.gestiones.models import Gestion
+from seguridad.salidas.models import Salida
+from seguridad.vehiculos.models import Vehiculo
+
+__all__ = ["Entrada", "Gestion", "Salida", "Vehiculo"]
