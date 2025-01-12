94e91e203fb339fb3277e89bc528083f09686ddb
Erick Perez <erickjpl@gmail.com>
2025-01-11 21:07:14 -0400

    clase de bootstrap en receptor

--- apps/presupuesto/receptor/forms.py
+++ apps/presupuesto/receptor/forms.py
@@ -4,6 +4,11 @@ from presupuesto.receptor.models import Receptor
 
 
 class ReceptorForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ReceptorForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({"class": "form-control"})
+
     class Meta:
         model = Receptor
         fields = (
