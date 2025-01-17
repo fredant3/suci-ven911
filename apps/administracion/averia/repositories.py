85a2674591e0fce08809c04235fef8cd5257f735
Erick Perez <34681565+erickjpl@users.noreply.github.com>
2025-01-17 12:54:39 -0400

    Update repositories.py

--- apps/administracion/averia/repositories.py
+++ apps/administracion/averia/repositories.py
@@ -1,6 +1,10 @@
-from administracion.averia.models import Averia
+from administracion.averia.models import Averia, TipoAveria
 from helpers.RepositoryMixin import Repository
 
+
+class TipoAveriaRepository(Repository):
+    def __init__(self):
+        self.entity = TipoAveria
 
 class AveriaRepository(Repository):
     def __init__(self):
