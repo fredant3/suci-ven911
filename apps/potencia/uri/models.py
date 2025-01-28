7d098566d42ddb77b6e1bef4f32e09fd328fe1f6
Sergio Herrera <sergiohre369@gmail.com>
2025-01-28 01:22:06 -0400

    Formulario URI parte 3

--- apps/potencia/uri/models.py
+++ apps/potencia/uri/models.py
@@ -42,6 +42,133 @@ ESTATUS_CHOICES6 = (
     ("otr", "Otro"),
 )
 
+ESTATUS_CHOICES7 = (
+    ("conduc", "Conductor"),
+    ("tripul", "Tripulante"),
+)
+
+ESTATUS_CHOICES8 = (
+    ("caipie", "Caida de Propio Pie"),
+    ("caialtu", "Caida de Altura"),
+    ("quemad", "Quemadura"),
+    ("morde", "Mordedura"),
+    ("otr", "Otros"),
+)
+
+ESTATUS_CHOICES9 = (
+    ("aler", "Alergias"),
+    ("cefa", "Cefalea"),
+    ("tension", "Hipo/Hipertensión"),
+    ("gine", "Gineco-obstetrica"),
+    ("conoci", "Perdida de Conocimiento"),
+    ("disne", "Disnea/Dificultad Respiratoria"),
+    ("dolo", "Dolor"),
+    ("emes", "Emesis"),
+    ("intox", "Intoxicaciones"),
+    ("shock", "Shock no traumatico"),
+    ("inme", "Inmersión"),
+    ("sca", "SCA"),
+    ("parad", "Parada Cardio Respiratoria"),
+    ("convul", "Convulsiones"),
+    ("hemor", "Hemorragias no traumaticas"),
+    ("otro", "Otros"),
+)
+
+ESTATUS_CHOICES10 = (
+    ("conscie", "Consciente"),
+    ("inconsc", "Inconsciente"),
+    ("viaperme", "V. A. Permeable"),
+    ("vianope", "V. A. No Permeable"),
+    ("ovac", "OVACE"),
+    ("liquid", "Liquidos/Fluidos"),
+    ("ruido", "Ruidos en V. A."),
+    ("rme1", "Necesita RME"),
+)
+
+ESTATUS_CHOICES11 = (
+    ("freme", "Frente-Menton"),
+    ("tracc", "Tracción Mandibular"),
+    ("succ", "Succión"),
+    ("rme2", "RME"),
+    ("cof", "COF"),
+    ("cnf", "CNF"),
+    ("dgs", "DSG/TET"),
+    ("vaq", "VAQ"),
+)
+
+ESTATUS_CHOICES12 = (
+    ("vaperme", "V. A. Permeabilizada"),
+    ("vadeso", "V. A. Desobstruida"),
+    ("movesp", "Mov. Esp. Restrigida"),
+    ("vaisla", "V. A. Aislada/Asegurada"),
+)
+
+ESTATUS_CHOICES13 = (
+    ("resp", "Respira/FR"),
+    ("noresp", "No Respira"),
+    ("Expsim", "Expansión Simetrica"),
+    ("expasi", "Expansión Asimétrica"),
+    ("esresp", "Esfuerzo Resp. Visible"),
+    ("heris", "Herida Succionante"),
+    ("estig", "Estigma de Trauma"),
+    ("desvit", "Desviación Traqueal"),
+    ("ingur", "Ingurgitación Yugular"),
+    ("crepo", "Crepitantes (óseos)"),
+    ("dolo", "Dolor"),
+    ("enfi", "Enfisema S.C"),
+    ("reso", "Resonancia"),
+    ("hipere", "Hiper-resonancia"),
+    ("mati", "Matidez"),
+    ("murmu", "Murmullo Vesicular"),
+    ("ronc", "Roncus"),
+    ("sibi", "Sibilancias"),
+    ("crepi", "Crepitantes"),
+    ("aboli", "Abolición/Disminución RR"),
+)
+
+ESTATUS_CHOICES14 = (
+    ("ventb", "Venta. Boca-Barr-Boca"),
+    ("ventco", "Venta. Con BVM"),
+    ("ventim", "Ventilación Mecánica"),
+    ("valv", "Valvula de una vía"),
+    ("parch", "Parche Oclusivo"),
+    ("descom", "Descomp. por Aguja"),
+    ("oxige", "Oxigenoterapia"),
+    ("disp", "Dispositivo"),
+    ("lpm", "LPM"),
+    ("otr", "Otro"),
+)
+
+ESTATUS_CHOICES15 = (
+    ("venties", "Ventila espontáneamente"),
+    ("ventia", "Ventila con Asistencia"),
+    ("neumot", "Neumotorax resuelto (Abierto o a tensión)"),
+    ("aliv", "Alivio de la disnea (de origen no traumático)"),
+)
+
+ESTATUS_CHOICES16 = (
+    ("pali", "Pálido"),
+    ("norm", "Normal"),
+)
+
+
+ESTATUS_CHOICES17 = (
+    ("fria", "Fría"),
+    ("tibi", "Tibia"),
+)
+
+ESTATUS_CHOICES18 = (
+    ("sec", "Seca"),
+    ("hume", "Húmeda"),
+)
+
+ESTATUS_CHOICES19 = (
+    ("isoc", "Isocórica"),
+    ("anis", "Anisocoria"),
+    ("mios", "Miosis"),
+    ("midri", "Midriasis"),
+)
+
 
 class Uri(BaseModel):
     # 1) Informacion General
@@ -203,6 +330,211 @@ class Uri(BaseModel):
 
     # 5) Informacion Clinica
     # Trauma
+    accidenteVehicular = models.CharField(
+        "Accidente Vehicular", max_length=9, choices=ESTATUS_CHOICES2
+    )  # opciones: (colision, volcamiento, Embarracamiento, Choque, Arrollamiento, otro )
+    enfrentamientoArmado = models.CharField(
+        "Enfrentamiento Armado", max_length=9, choices=ESTATUS_CHOICES2
+    )  # si es si, activar opciones "Tipo de Arma" (Blanca, Fuego, Biologica, Quimica, Otro)
+    # tipoArma=models.ForeignKey(Tipo_arma, on_delete=models.CASCADE, blank=True, null=True)
+    traumaVehiculo = models.CharField(
+        "Trauma con Vehiculo", max_length=9, choices=ESTATUS_CHOICES2
+    )  # si es si, activar opciones "Tipo de Vehiculo" (Automovil/Carro, Moto, Maquinaria, Otro)
+    viajaba = models.CharField("Viajaba como", max_length=9, choices=ESTATUS_CHOICES7)
+    sustanciaPeligrosa = models.CharField(
+        "Sustancia Peligrosa", max_length=9, choices=ESTATUS_CHOICES2
+    )  # si es si, activar opcion si es inflamable o explosivo)
+    # inflamable = models.BooleanField()
+    # explosivo = models.BooleanField()
+    observacionesSustancia = models.CharField(
+        "Observaciones de la sustancia", max_length=100, blank=True, null=True
+    )
+    traumaNoIntencional = models.CharField(
+        "Trauma no Intencional", max_length=9, choices=ESTATUS_CHOICES8
+    )
+    emergenciaMedica = models.CharField(
+        "Emergencias Médicas no Traumáticas", max_length=9, choices=ESTATUS_CHOICES9
+    )
+
+    # Evaluacion Inicial + Intervenciones criticas
+
+    hemorragia = models.CharField("Hemorragias", max_length=9, choices=ESTATUS_CHOICES2)
+    presion = models.CharField(
+        "Presión Directa", max_length=9, choices=ESTATUS_CHOICES2
+    )
+    empaquetado = models.CharField(
+        "Empaquetado de la Herida", max_length=9, choices=ESTATUS_CHOICES2
+    )
+    torniquete = models.CharField("Torniquete", max_length=9, choices=ESTATUS_CHOICES2)
+
+    # Via aerea o control cervical
+
+    evaluacion = models.CharField("Evaluación", max_length=9, choices=ESTATUS_CHOICES10)
+    intervencion = models.CharField(
+        "Intervenciones", max_length=9, choices=ESTATUS_CHOICES11
+    )
+    resultado = models.CharField("Resultados", max_length=9, choices=ESTATUS_CHOICES12)
+    descripcion_adic = models.CharField(
+        "Descripción Adicional", max_length=100, blank=True, null=True
+    )
+
+    # 6) Respiracion, Oxigenacion y Circulacion
+
+    # Respiracion y Oxigenacion
+
+    evaluacionResp = models.CharField(
+        "Evaluación", max_length=20, choices=ESTATUS_CHOICES13
+    )
+    intervencionResp = models.CharField(
+        "Intervención", max_length=20, choices=ESTATUS_CHOICES14
+    )
+    resultadoResp = models.CharField(
+        "Resultado", max_length=20, choices=ESTATUS_CHOICES15
+    )
+    descripcion_adic_resp = models.CharField(
+        "Descripción Adicional", max_length=100, blank=True, null=True
+    )
+
+    # Circulacion
+    colorPiel = models.CharField(
+        "Color de la Piel", max_length=9, choices=ESTATUS_CHOICES16
+    )
+    temperaturaPiel = models.CharField(
+        "Temperatura de la Piel", max_length=9, choices=ESTATUS_CHOICES17
+    )
+    humedadPiel = models.CharField(
+        "Humedad de la Piel", max_length=9, choices=ESTATUS_CHOICES18
+    )
+    pulso = models.CharField("Pulsos distales", max_length=9, choices=ESTATUS_CHOICES2)
+    otrasHerida = models.CharField(
+        "Otras hemorro/heridas", max_length=9, choices=ESTATUS_CHOICES2
+    )
+    fractura = models.CharField("Fracturas", max_length=9, choices=ESTATUS_CHOICES2)
+    maniobraPelvis = models.CharField(
+        "Maniobra de Pelvis", max_length=9, choices=ESTATUS_CHOICES2
+    )
+
+    # Deficit Neurologico
+    ecgO = models.IntegerField(blank=True, null=True)
+    ecgV = models.IntegerField(blank=True, null=True)
+    ecgM = models.IntegerField(blank=True, null=True)
+    ecgTotal = models.IntegerField(blank=True, null=True)
+    reaccionPupilar = models.CharField(
+        "Reacción Pupilar", max_length=9, choices=ESTATUS_CHOICES19
+    )
+
+    # Exposicion
+    hipotermia = models.CharField(
+        "Hipotermia", max_length=9, choices=ESTATUS_CHOICES2
+    )  # Opciones si es positivo (Menta Termica, Admini de liq tibios EV, otras lesiones)
+    signosSintomas = models.CharField(
+        "Signos y Sintomas", max_length=100, blank=True, null=True
+    )
+    alergias = models.CharField("Alergias", max_length=100, blank=True, null=True)
+    medicamentos = models.CharField(
+        "Medicamentos", max_length=100, blank=True, null=True
+    )
+    preexistencias = models.CharField(
+        "Preexistencias", max_length=100, blank=True, null=True
+    )
+    ultimaComida = models.CharField(
+        "Última comida", max_length=100, blank=True, null=True
+    )
+    evento = models.CharField("Evento", max_length=100, blank=True, null=True)
+
+    # 7) Signos Vitales o Tratamiento
+
+    # Signos Vitales
+    horaMedicion = models.CharField(
+        "Hora de la Medición", max_length=100, blank=True, null=True
+    )
+    frecuenciaCardiaca = models.CharField(
+        "Frecuencia Cardiaca", max_length=100, blank=True, null=True
+    )
+    frecuenciaRespiratoria = models.CharField(
+        "Frecuencia Respiratoria", max_length=100, blank=True, null=True
+    )
+    presionArterial = models.CharField(
+        "Presión Arterial", max_length=100, blank=True, null=True
+    )
+    spo2 = models.CharField("SPO2", max_length=100, blank=True, null=True)
+    temperatura = models.CharField("Temperatura", max_length=100, blank=True, null=True)
+    llenadoCapilar = models.CharField(
+        "Llenado Capilar", max_length=100, blank=True, null=True
+    )
+    glicemiaCapilar = models.CharField(
+        "Glicemia Capillar", max_length=100, blank=True, null=True
+    )
+    escalaGlasgow = models.CharField(
+        "Escala Glasgow", max_length=100, blank=True, null=True
+    )
+
+    # Tratamiento (opcion para añadir/ adicionar)
+    medicamento = models.CharField("Medicamento", max_length=100, blank=True, null=True)
+    dosis = models.CharField("Dosis", max_length=100, blank=True, null=True)
+    hora = models.TimeField("Hora", blank=True, null=True)
+    resultadoEvaluacion = models.CharField(
+        "Resultados de la Evaluación Fisica Cefalo Caudal",
+        max_length=500,
+        blank=True,
+        null=True,
+    )
+
+    # Registro de Referencias y Contrareferencias
+
+    trasladoIncial = models.CharField(
+        "¿El paciente fue referido del centro asistencial al que fue trasladado inicialmente?",
+        max_length=9,
+        choices=ESTATUS_CHOICES2,
+    )
+    hospitalOrigen = models.CharField(
+        "Hospital Origen", max_length=100, blank=True, null=True
+    )
+    medicoRefiere = models.CharField(
+        "Medico que refiere", max_length=100, blank=True, null=True
+    )
+    horaSalidaHosp = models.TimeField("Hora de Salida", blank=True, null=True)
+    hospitalDestino = models.CharField(
+        "Hospital que recibe", max_length=100, blank=True, null=True
+    )
+    horaLlegadaHosp = models.TimeField("Hora de llegada", blank=True, null=True)
+    causa = models.CharField(
+        "Ingreses las causas", max_length=100, blank=True, null=True
+    )
+    tecnicoEmergencia = models.CharField(
+        "Técnico de emergencias médicas", max_length=50, blank=True, null=True
+    )
+    cedulaTecnico = models.CharField(
+        "Cédula del Técnico de emergencias médicas",
+        max_length=10,
+        blank=True,
+        null=True,
+    )
+    tercerTripulante = models.CharField(
+        "Tercer Tripulante", max_length=50, blank=True, null=True
+    )
+    cedulaTripulante = models.CharField(
+        "Cédula del tercer tripulante", max_length=10, blank=True, null=True
+    )
+    conductorUnidad = models.CharField(
+        "Conductor de la unidad", max_length=50, blank=True, null=True
+    )
+    cedulaConductor = models.CharField(
+        "Cedula del conductor de la unidad", max_length=10, blank=True, null=True
+    )
+    supervisorGuardia = models.CharField(
+        "Supervisor de Guardia", max_length=50, blank=True, null=True
+    )
+    cedulaSupervisor = models.CharField(
+        "Cédula del Supervisor de Guardia", max_length=10, blank=True, null=True
+    )
+    medicoGuardia = models.CharField(
+        "Médico de Guardia", max_length=50, blank=True, null=True
+    )
+    cedulaMedico = models.CharField(
+        "Cédula del Médico de Guardia", max_length=10, blank=True, null=True
+    )
+    selloMsds = models.CharField("Sellos/MSDS", max_length=25, blank=True, null=True)
 
     def toJSON(self):
         return model_to_dict(self)
