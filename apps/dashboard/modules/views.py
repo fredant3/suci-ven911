03c3a962b1d37cdc818b78f3a517852ff8cee08b
Erick Perez <erickjpl@gmail.com>
2025-03-01 19:07:05 -0400

    Tecnologia Submodulos

--- apps/dashboard/modules/views.py
+++ apps/dashboard/modules/views.py
@@ -73,7 +73,7 @@ class Modules(LoginRequiredMixin, TemplateView):
             },
             {
                 "title": "Tecnología Comunicación e Información (Pendiente)",
-                "url": "tecnologia:list",
+                "url": "tecnologia",
                 "image": "img/modules/tecnologia.png",
             },
             {
