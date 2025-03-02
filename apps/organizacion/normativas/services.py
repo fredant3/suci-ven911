8458843d16df611ef0fddd5d282f2617eba29776
Erick Perez <erickjpl@gmail.com>
2025-03-02 16:50:30 -0400

    biblioteca

--- apps/organizacion/normativas/services.py
+++ apps/organizacion/normativas/services.py
@@ -1,5 +1,5 @@
+from django.conf import settings
 from helpers.CrudMixin import CrudService
-
 from organizacion.normativas.repositories import NormativaRepository
 
 
@@ -18,3 +18,22 @@ class NormativaService(CrudService):
         file = read.file
         if file:
             self.repository.remove_media(file.path)
+
+    def getAll(
+        self, draw, start, length, search=None, orderBy=None, orderAsc=None, select=("")
+    ):
+        select = select if select else self.select
+        if search is None:
+            entities = self.repository.getAll(select, orderBy, orderAsc)
+        else:
+            search = self.criteria(search)
+            entities = self.repository.getFilter(search, select, orderBy, orderAsc)
+
+        for field in entities:
+            if "file" in field:
+                url_file = f"{settings.MEDIA_URL}{field['file']}"
+                field["file"] = (
+                    f'<img src="{url_file}" style="height:50px;width:50px;" />'
+                )
+
+        return self.response(entities, start, length, draw)
