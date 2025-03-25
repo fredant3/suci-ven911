5a7f5e5f4931a06471a00d625ebad0985a48b8b5
Sergio Herrera <sergiohre369@gmail.com>
2025-03-25 14:35:28 -0400

    Correciones en choices de potencia frontend

--- apps/potencia/incidencias/models.py
+++ apps/potencia/incidencias/models.py
@@ -6,17 +6,42 @@ from helpers.BaseModelMixin import BaseModel, ESTADOS_CHOICES
 from potencia.tipo_incidencia.models import TipoIncidencia
 
 INCIDENCIA_CHOICES = (
-    ("soliint", "Solicitud Interna"),
-    ("soliext", "Solicitud Externa"),
+    ("Interna", "Solicitud Interna"),
+    ("Externa", "Solicitud Externa"),
+)
+
+ESTADOS_CHOICES = (
+    ("Dtto. Capital", "Distrito Capital"),
+    ("Amazonas", "Amazonas"),
+    ("Anzoátegui", "Anzoátegui"),
+    ("Apure", "Apure"),
+    ("Aragua", "Aragua"),
+    ("Barinas", "Barinas"),
+    ("Bolívar", "Bolívar"),
+    ("Carabobo", "Carabobo"),
+    ("Cojedes", "Cojedes"),
+    ("Delta Amacuro", "Delta Amacuro"),
+    ("Falcón", "Falcón"),
+    ("Guárico", "Guárico"),
+    ("Lara", "Lara"),
+    ("Mérida", "Mérida"),
+    ("Miranda", "Miranda"),
+    ("Monagas", "Monagas"),
+    ("Nva. Esparta", "Nueva Esparta"),
+    ("Portuguesa", "Portuguesa"),
+    ("Sucre", "Sucre"),
+    ("Táchira", "Táchira"),
+    ("Trujillo", "Trujillo"),
+    ("Vargas", "Vargas"),
+    ("Yaracuy", "Yaracuy"),
+    ("Zulia", "Zulia"),
 )
 
 
 class Incidencia(BaseModel):
     sede = models.ForeignKey(Sede, on_delete=models.CASCADE)
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
-    estado = models.CharField(
-        "Estado", name="estado", max_length=2, choices=ESTADOS_CHOICES
-    )
+    estado = models.CharField("Estado", max_length=15, choices=ESTADOS_CHOICES)
     tipo_incidencia = models.ForeignKey(TipoIncidencia, on_delete=models.CASCADE)
     tipo_solicitud = models.CharField(
         "Tipo de Solicitud", max_length=10, choices=INCIDENCIA_CHOICES
