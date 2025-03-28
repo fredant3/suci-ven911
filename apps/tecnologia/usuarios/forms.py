838568a7e1f6fff792c5cc9aeafcaba38facedde
jesusxavier <jesusxav831@gmail.com>
2025-03-28 01:15:09 -0400

    express

--- apps/tecnologia/usuarios/forms.py
+++ apps/tecnologia/usuarios/forms.py
@@ -1,8 +1,14 @@
+from django.forms import BooleanField, CheckboxInput
 from users.auth.models import User
 from helpers.FormBase import FormBase
 
 
-class UserForm(FormBase):
+class UserForm(FormBase):        
+    is_active = BooleanField(
+        required=False,
+        widget=CheckboxInput(attrs={"value": "False"}),
+    )
+
     class Meta:
         model = User
         fields = (
