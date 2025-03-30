02781fe4c42dd4415f70d3701468995aab135a46
jose Esteves <joseesteves199930@gmail.com>
2025-03-30 16:06:06 -0400

    mejorar textos y placeholders en formularios de averías

--- apps/administracion/averia/views/update_view.py
+++ apps/administracion/averia/views/update_view.py
@@ -17,12 +17,12 @@ class AveriaUpdateView(LoginRequiredMixin, CheckPermisosMixin, UpdateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["titlePage"] = "administracion"
+        context["titlePage"] = "Administración"
         context["indexUrl"] = reverse_lazy("administracion")
-        context["module"] = "Administracion"
-        context["submodule"] = "Averia"
-        context["titleForm"] = "Actualizar"
-        context["tag"] = "Editar"
+        context["module"] = "Administración"
+        context["submodule"] = "Averías"
+        context["titleForm"] = "Editar avería"
+        context["tag"] = "Guardar cambios"
         context["listUrl"] = reverse_lazy("averias:list")
         context["urlForm"] = reverse_lazy(
             "api_averias:update", args=[self.kwargs.get("pk")]
