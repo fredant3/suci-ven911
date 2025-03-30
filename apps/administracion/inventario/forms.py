1a4afadab05b5f269be720040ee691dd12f47610
jose Esteves <joseesteves199930@gmail.com>
2025-03-30 18:45:03 -0400

    mejorar textos y etiquetas en vistas de artículos para consistencia y claridad

--- apps/administracion/inventario/forms.py
+++ apps/administracion/inventario/forms.py
@@ -1,173 +1,75 @@
 from administracion.inventario.models import Articulo
 from helpers.FormBase import FormBase
-from helpers.validForm import validate_decimal_number
-from django import forms
+from django.forms import TextInput, NumberInput, IntegerField, HiddenInput
 
 
 class ArticuloForm(FormBase):
     fecha_adq = FormBase.create_date_field("fecha_adq")
-    tipo_articulo_id = forms.IntegerField(widget=forms.HiddenInput(), required=False)
 
     class Meta:
         model = Articulo
         fields = [
-            "tipo_articulo_id",
             "descripcion",
             "marca",
             "modelo",
             "serial",
+            "codigo_bn",
+            "cantidad",
+            "condicion",
+            "fecha_adq",
+        ]
+        widgets = {
+            "descripcion": TextInput(
+                attrs={"placeholder": "Ingrese la descripción del artículo"}
+            ),
+            "marca": TextInput(attrs={"placeholder": "Ingrese la marca del artículo"}),
+            "modelo": TextInput(
+                attrs={"placeholder": "Ingrese el modelo del artículo"}
+            ),
+            "serial": TextInput(attrs={"placeholder": "Ingrese el número de serie"}),
+            "codigo_bn": TextInput(
+                attrs={"placeholder": "Ingrese el código de bienes nacionales"}
+            ),
+            "cantidad": NumberInput(
+                attrs={"placeholder": "Ingrese la cantidad disponible", "min": 1}
+            ),
+        }
+
+
+class TecnologiaForm(ArticuloForm):
+    pass
+
+
+class ConsumibleForm(ArticuloForm):
+    pass
+
+
+class MobiliarioForm(ArticuloForm):
+    class Meta(ArticuloForm.Meta):
+        fields = [
+            "descripcion",
+            "serial",
+            "codigo_bn",
+            "cantidad",
+            "condicion",
+            "fecha_adq",
+        ]
+
+
+class VehiculoForm(ArticuloForm):
+    placa = IntegerField(
+        label="Placa",
+        widget=TextInput(attrs={"placeholder": "Ingrese la placa del vehículo"}),
+    )
+    cantidad_combustible = IntegerField(
+        label="Cantidad de combustible",
+        widget=NumberInput(
+            attrs={"placeholder": "Ingrese la cantidad de combustible", "min": 0}
+        ),
+    )
+
+    class Meta(ArticuloForm.Meta):
+        fields = ArticuloForm.Meta.fields + [
             "placa",
             "cantidad_combustible",
-            "codigo_bn",
-            "cantidad",
-            "condicion",
-            "fecha_adq",
         ]
-        widgets = {
-            "descripcion": forms.TextInput(
-                attrs={"placeholder": "Ingrese la descripción"}
-            ),
-            "marca": forms.TextInput(attrs={"placeholder": "Ingrese la marca"}),
-            "modelo": forms.TextInput(attrs={"placeholder": "Ingrese el modelo"}),
-            "serial": forms.TextInput(attrs={"placeholder": "Ingrese el serial"}),
-            "placa": forms.TextInput(attrs={"placeholder": "Ingrese la placa"}),
-            "cantidad_combustible": forms.NumberInput(
-                attrs={"placeholder": "Ingrese la cantidad de combustible"}
-            ),
-            "codigo_bn": forms.TextInput(attrs={"placeholder": "Ingrese el código BN"}),
-            "cantidad": forms.NumberInput(attrs={"placeholder": "Ingrese la cantidad"}),
-            "condicion": forms.Select(attrs={"placeholder": "Ingrese la condición"}),
-            "fecha_adq": forms.DateInput(
-                attrs={"placeholder": "Seleccione la fecha de adquisición"}
-            ),
-        }
-
-    # def clean_cantidad_combustible(self):
-    #     cantidad_combustible = self.cleaned_data.get("cantidad_combustible")
-    #     validate_decimal_number(
-    #         cantidad_combustible,
-    #         "La cantidad de combustible debe ser un número positivo.",
-    #     )
-    #     return cantidad_combustible
-
-
-class TecnologiaForm(FormBase):
-    fecha_adq = FormBase.create_date_field("fecha_adq", title="Fecha de adquisición")
-
-    class Meta:
-        model = Articulo
-        fields = [
-            "descripcion",
-            "marca",
-            "modelo",
-            "serial",
-            "codigo_bn",
-            "cantidad",
-            "condicion",
-            "fecha_adq",
-        ]
-        widgets = {
-            "descripcion": forms.TextInput(
-                attrs={"placeholder": "Ingrese la descripción"}
-            ),
-            "marca": forms.TextInput(attrs={"placeholder": "Ingrese la marca"}),
-            "modelo": forms.TextInput(attrs={"placeholder": "Ingrese el modelo"}),
-            "serial": forms.TextInput(attrs={"placeholder": "Ingrese el serial"}),
-            "codigo_bn": forms.TextInput(attrs={"placeholder": "Ingrese el código BN"}),
-            "cantidad": forms.NumberInput(attrs={"placeholder": "Ingrese la cantidad"}),
-            "condicion": forms.Select(attrs={"placeholder": "Ingrese la condición"}),
-            "fecha_adq": forms.DateInput(
-                attrs={
-                    "placeholder": "Seleccione la fecha de adquisición",
-                }
-            ),
-        }
-
-
-class ConsumibleForm(FormBase):
-    fecha_adq = FormBase.create_date_field("fecha_adq", title="Fecha de adquisición")
-
-    class Meta:
-        model = Articulo
-        fields = ["descripcion", "marca", "serial", "cantidad", "fecha_adq"]
-        widgets = {
-            "descripcion": forms.TextInput(
-                attrs={"placeholder": "Ingrese la descripción"}
-            ),
-            "marca": forms.TextInput(attrs={"placeholder": "Ingrese la marca"}),
-            "serial": forms.TextInput(attrs={"placeholder": "Ingrese el serial"}),
-            "cantidad": forms.NumberInput(attrs={"placeholder": "Ingrese la cantidad"}),
-            "fecha_adq": forms.DateInput(
-                attrs={"placeholder": "Seleccione la fecha de adquisición"}
-            ),
-        }
-
-
-class MobiliarioForm(FormBase):
-    fecha_adq = FormBase.create_date_field("fecha_adq", title="Fecha de adquisición")
-
-    class Meta:
-        model = Articulo
-        fields = [
-            "descripcion",
-            "serial",
-            "codigo_bn",
-            "cantidad",
-            "condicion",
-            "fecha_adq",
-        ]
-        widgets = {
-            "descripcion": forms.TextInput(
-                attrs={"placeholder": "Ingrese la descripción"}
-            ),
-            "serial": forms.TextInput(attrs={"placeholder": "Ingrese el serial"}),
-            "codigo_bn": forms.TextInput(attrs={"placeholder": "Ingrese el código BN"}),
-            "cantidad": forms.NumberInput(attrs={"placeholder": "Ingrese la cantidad"}),
-            "condicion": forms.Select(attrs={"placeholder": "Ingrese la condición"}),
-            "fecha_adq": forms.DateInput(
-                attrs={"placeholder": "Seleccione la fecha de adquisición"}
-            ),
-        }
-
-
-class VehiculoForm(FormBase):
-    fecha_adq = FormBase.create_date_field("fecha_adq", title="Fecha de adquisición")
-
-    class Meta:
-        model = Articulo
-        fields = [
-            "descripcion",
-            "marca",
-            "modelo",
-            "placa",
-            "cantidad_combustible",
-            "codigo_bn",
-            "cantidad",
-            "condicion",
-            "fecha_adq",
-        ]
-        widgets = {
-            "descripcion": forms.TextInput(
-                attrs={"placeholder": "Ingrese la descripción"}
-            ),
-            "marca": forms.TextInput(attrs={"placeholder": "Ingrese la marca"}),
-            "modelo": forms.TextInput(attrs={"placeholder": "Ingrese el modelo"}),
-            "placa": forms.TextInput(attrs={"placeholder": "Ingrese la placa"}),
-            "cantidad_combustible": forms.NumberInput(
-                attrs={"placeholder": "Ingrese la cantidad de combustible"}
-            ),
-            "codigo_bn": forms.TextInput(attrs={"placeholder": "Ingrese el código BN"}),
-            "cantidad": forms.NumberInput(attrs={"placeholder": "Ingrese la cantidad"}),
-            "condicion": forms.Select(attrs={"placeholder": "Ingrese la condición"}),
-            "fecha_adq": forms.DateInput(
-                attrs={"placeholder": "Seleccione la fecha de adquisición"}
-            ),
-        }
-
-    def clean_cantidad_combustible(self):
-        cantidad_combustible = self.cleaned_data.get("cantidad_combustible")
-        validate_decimal_number(
-            cantidad_combustible,
-            "La cantidad de combustible debe ser un número positivo.",
-        )
-        return cantidad_combustible
