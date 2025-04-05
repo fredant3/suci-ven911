601257cb0a98e5d854928059cc7631588faf1ada
Erick Perez <erickjpl@gmail.com>
2025-04-05 19:23:09 -0400

    eliminando archivos innecesarios de rrhh

--- apps/rrhh/urls.py
+++ apps/rrhh/urls.py
@@ -3,30 +3,10 @@ from apps.rrhh.views import GestionHumanaView
 
 urlpatterns = [
     path("", GestionHumanaView.as_view(), name="gestion_humana"),
-    path("empleados/", include(("apps.rrhh.empleados.urls", "empleados"))),
-    path(
-        "api/empleados/",
-        include(("apps.rrhh.empleados.urls_apis", "api_empleados")),
-    ),
     path("cargos/", include(("apps.rrhh.cargos.urls", "cargos"))),
     path(
         "api/cargos/",
         include(("apps.rrhh.cargos.urls_apis", "api_cargos")),
-    ),
-    path("dotaciones/", include(("apps.rrhh.dotaciones.urls", "dotaciones"))),
-    path(
-        "api/dotaciones/",
-        include(("apps.rrhh.dotaciones.urls_apis", "api_dotaciones")),
-    ),
-    path("educaciones/", include(("apps.rrhh.educaciones.urls", "educaciones"))),
-    path(
-        "api/educaciones/",
-        include(("apps.rrhh.educaciones.urls_apis", "api_educaciones")),
-    ),
-    path("familiares/", include(("apps.rrhh.familiares.urls", "familiares"))),
-    path(
-        "api/familiares/",
-        include(("apps.rrhh.familiares.urls_apis", "api_familiares")),
     ),
     path("sueldos/tipos", include(("apps.rrhh.tipos_sueldos.urls", "tipos_sueldos"))),
     path(
