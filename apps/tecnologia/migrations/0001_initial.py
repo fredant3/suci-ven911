aa2e8162c963ea7457b9a7df90d89f134ba45923
Freddy <144859937+fredant3@users.noreply.github.com>
2025-04-11 15:34:52 -0400

    arreglando errores

---
+++ apps/tecnologia/migrations/0001_initial.py
@@ -0,0 +1,46 @@
+import django.db.models.deletion
+from django.conf import settings
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name="RequestLog",
+            fields=[
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("ip_address", models.CharField(max_length=50)),
+                ("device_type", models.CharField(max_length=50)),
+                ("device_name", models.CharField(max_length=100)),
+                ("operating_system", models.CharField(max_length=100)),
+                ("browser", models.CharField(max_length=100)),
+                ("method", models.CharField(max_length=10)),
+                ("url", models.URLField(max_length=500)),
+                ("timestamp", models.DateTimeField(auto_now_add=True)),
+                (
+                    "user",
+                    models.ForeignKey(
+                        blank=True,
+                        null=True,
+                        on_delete=django.db.models.deletion.SET_NULL,
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
+            ],
+        ),
+    ]
