b19eeb5860de6cc6a5de701e51c2e08c6a16cb27
jose Esteves <joseesteves199930@gmail.com>
2025-04-14 19:10:24 -0400

    Agregar criterios de búsqueda y método de listado en el servicio de incidencias

--- apps/potencia/incidencias/services.py
+++ apps/potencia/incidencias/services.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from administracion.departamentos.repositories import DepartamentoRepository
 from administracion.sedes.repositories import SedeRepository
 from helpers.CrudMixin import CrudService
@@ -15,6 +16,7 @@ class IncidenciaService(CrudService):
         "sede__sede",
         "departamento__nombre",
         "tipo_incidencia__tipo",
+        "observaciones",
     )
 
     def __init__(self):
@@ -39,7 +41,25 @@ class IncidenciaService(CrudService):
         payload["tipo_incidencia"] = self.search_tipo_incidencia(
             payload["tipo_incidencia"]
         )
-
         return payload
 
     # END Create
+
+    def criteria(self, search, arg=None):
+        query = Q()
+
+        if search:
+            query &= (
+                Q(sede__sede__icontains=search)
+                | Q(departamento__nombre__icontains=search)
+                | Q(tipo_incidencia__tipo__icontains=search)
+                | Q(estado__icontains=search)
+                | Q(tipo_solicitud__icontains=search)
+                | Q(observaciones__icontains=search)
+            )
+
+        return query
+
+    def list(self, search=None, page=None, per_page=None, arg=None):
+        query = self.criteria(search, arg)
+        return self.repository.list(query, self.select, page, per_page)
