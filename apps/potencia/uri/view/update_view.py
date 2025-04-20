ccadfee647f97c20ef0726cfcc9d00b1f1df365c
jose Esteves <joseesteves199930@gmail.com>
2025-04-20 19:15:03 -0400

    Renombrar UriUpdateView a UriUpdateWizardView y actualizar lógica para manejo de formularios en múltiples pasos.

--- apps/potencia/uri/view/update_view.py
+++ apps/potencia/uri/view/update_view.py
@@ -1,14 +1,15 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import UpdateView
+from formtools.wizard.views import SessionWizardView
+from helpers.CheckPermisosMixin import CheckPermisosMixin
+from templates.sneat import TemplateLayout
+from django.http import HttpResponseRedirect
+
 from helpers.CheckPermisosMixin import CheckPermisosMixin
 from helpers.ControllerMixin import UpdateController
+
 from potencia.uri.models import Uri
-from potencia.uri.services import UriService
-
-from templates.sneat import TemplateLayout
-
-from ..forms import (
+from potencia.uri.forms import (
     UriInfoGeneralForm,
     UriInfolegalForm,
     UripacienteForm,
@@ -20,35 +21,56 @@ from ..forms import (
 )
 
 
-class UriUpdateView(LoginRequiredMixin, CheckPermisosMixin, UpdateView):
+# TODO: probar
+class UriUpdateWizardView(LoginRequiredMixin, CheckPermisosMixin, SessionWizardView):
     permission_required = "potencia.uri.editar_uri"
-    form_class = UripacienteForm
+    template_name = "widzard/index.html"
+    form_list = [
+        ("general", UriInfoGeneralForm),
+        ("legal", UriInfolegalForm),
+        ("paciente", UripacienteForm),
+        ("consentimiento", UriConsentimientoForm),
+        ("direccion", UriDireccionForm),
+        ("clinica", UriInfoclinicaForm),
+        ("signos", UriSignosVitalesForm),
+        ("referencias", UriReferenciasForm),
+    ]
 
-    template_name = "sneat/layout/partials/form/layout.html"
+    def get_object(self):
+        uri_id = self.kwargs.get("pk")
+        return Uri.objects.get(pk=uri_id)
+
+    def get_form_instance(self, step):
+        uri = self.get_object()
+        return uri  # Todas las forms comparten la misma instancia Uri
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["titlePage"] = "URI"
+        context["titlePage"] = "Actualizar URI"
         context["indexUrl"] = reverse_lazy("modules:index")
         context["module"] = "URI"
         context["submodule"] = "Unidad de Respuesta Inmediata"
-        context["titleForm"] = "Actualizar registro"
-        context["tag"] = "Editar"
-        context["listUrl"] = reverse_lazy("uri:list")
-        context["urlForm"] = reverse_lazy(
-            "api_uri:update", args=[self.kwargs.get("pk")]
-        )
-        context["methodForm"] = "PUT"
+        context["action"] = "update"
+        context["uri_id"] = self.kwargs.get("pk")
         return TemplateLayout.init(self, context)
 
-    def get_queryset(self):
-        id = self.kwargs.get("pk")
-        return Uri.objects.filter(pk=id)
+    def done(self, form_list, form_dict, **kwargs):
+        uri = self.get_object()
+
+        # Actualizar todos los campos de todas las forms
+        for form in form_dict.values():
+            for field, value in form.cleaned_data.items():
+                setattr(uri, field, value)
+
+        uri.save()
+        return HttpResponseRedirect("/potencia/unidad-respuesta-inmediata")
 
 
 class UriUpdateApiView(UpdateController, CheckPermisosMixin):
     permission_required = "potencia.uri.editar_uri"
-    form_class = UripacienteForm
+    form_class = UriInfoGeneralForm
 
     def __init__(self):
+        from potencia.uri.services import UriService
+
         self.service = UriService()
