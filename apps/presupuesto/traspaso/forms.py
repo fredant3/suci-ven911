9546829c72d9330018276b5d29fd6f213f3703d8
jose Esteves <joseesteves199930@gmail.com>
2025-05-10 18:51:34 -0400

    Eliminar importación innecesaria de PROYECTOS_ACCIONES y limpiar widgets en TraspasoForm.

--- apps/presupuesto/traspaso/forms.py
+++ apps/presupuesto/traspaso/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from presupuesto.traspaso.models import PROYECTOS_ACCIONES, Traspaso
+from presupuesto.traspaso.models import Traspaso
 from presupuesto.cedente.models import Cedente
 from helpers.FormBase import FormBase
 
@@ -18,11 +18,7 @@ class TraspasoForm(FormBase):
             "deleted_at",
             "deleted_by",
         ]
-        widgets = {
-            # "proyecto_acciones": forms.Select(
-            #     choices=PROYECTOS_ACCIONES
-            # )
-        }
+        widgets = {}
 
     def save(self, commit=True):
         instance = super().save(commit=False)
