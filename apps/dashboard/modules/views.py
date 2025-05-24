c263f6a6d5b10e37ef37243b6a80c5d5a7182c11
Erick Perez <erickjpl@gmail.com>
2025-05-24 19:46:40 -0400

    varios permisos

--- apps/dashboard/modules/views.py
+++ apps/dashboard/modules/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
+from helpers.CheckPermisosMixin import Permisos
 
 from templates.sneat import TemplateLayout
 
@@ -8,21 +9,6 @@ class Modules(LoginRequiredMixin, TemplateView):
     login_url = "auth:login"
     template_name = "public/modules/index.html"
 
-    PERMISSIONS_ASESORIA = (
-        "asesoria.agregar_denuncia",
-        "asesoria.editar_denuncia",
-        "asesoria.eliminar_denuncia",
-        "asesoria.exel_denuncia",
-        "asesoria.listar_denuncia",
-        "asesoria.ver_denuncia",
-        "asesoria.agregar_registro_filmico",
-        "asesoria.editar_registro_filmico",
-        "asesoria.eliminar_registro_filmico",
-        "asesoria.exel_registro_filmico",
-        "asesoria.listar_registro_filmico",
-        "asesoria.ver_registro_filmico",
-    )
-
     def get_context_data(self, **kwargs):
         context = TemplateLayout.init(self, super().get_context_data(**kwargs))
         context["modules"] = [
@@ -30,131 +16,103 @@ class Modules(LoginRequiredMixin, TemplateView):
                 "title": "Asesoría Jurídica",
                 "url": "asesoria",
                 "image": "img/modules/asesoria_juridica.png",
-                "user_has_access": any(
-                    self.request.user.has_perm(perm)
-                    for perm in self.PERMISSIONS_ASESORIA
-                )
-                or self.request.user.is_superuser,
+                "user_has_access": Permisos.check_permissions(
+                    self.request.user, "asesoria"
+                ),
             },
             {
                 "title": "Gestión Humana",
                 "url": "gestion_humana",
                 "image": "img/modules/gestion_humana.png",
-                "user_has_access": self.request.user.is_superuser
-                or any(
-                    self.request.user.has_perm(perm)
-                    for perm in self.PERMISSIONS_ASESORIA
+                "user_has_access": Permisos.check_permissions(
+                    self.request.user, "gestion_humana"
                 ),
             },
             {
                 "title": "Gestión Administrativa",
                 "url": "administracion",
                 "image": "img/modules/gestion_administrativa.png",
-                "user_has_access": self.request.user.is_superuser,
-                # or any(
-                #     self.request.user.has_perm(perm)
-                #     for perm in self.PERMISSIONS_ASESORIA
-                # ),
+                "user_has_access": Permisos.check_permissions(
+                    self.request.user, "administracion"
+                ),
             },
             {
                 "title": "Unidad de Respuesta Inmediata",
                 "url": "uri:list",
                 "image": "img/modules/ambulancias.png",
-                "user_has_access": self.request.user.is_superuser,
-                # or any(
-                #     self.request.user.has_perm(perm)
-                #     for perm in self.PERMISSIONS_ASESORIA
-                # ),
+                "user_has_access": Permisos.check_permissions(self.request.user, "uri"),
             },
             {
                 "title": "Potencia",
                 "url": "potencia",
                 "image": "img/modules/incidente.png",
-                "user_has_access": self.request.user.is_superuser,
-                # or any(
-                #     self.request.user.has_perm(perm)
-                #     for perm in self.PERMISSIONS_ASESORIA
-                # ),
+                "user_has_access": Permisos.check_permissions(
+                    self.request.user, "potencia"
+                ),
             },
             {
                 "title": "Organización",
                 "url": "organizacion",
                 "image": "img/modules/organizacion.png",
-                "user_has_access": self.request.user.is_superuser,
-                # or any(
-                #     self.request.user.has_perm(perm)
-                #     for perm in self.PERMISSIONS_ASESORIA
-                # ),
+                "user_has_access": Permisos.check_permissions(
+                    self.request.user, "asesoria"
+                ),
             },
             {
                 "title": "Presupuestos",
                 "url": "presupuesto",
                 "image": "img/modules/presupuesto.png",
-                "user_has_access": self.request.user.is_superuser,
-                # or any(
-                #     self.request.user.has_perm(perm)
-                #     for perm in self.PERMISSIONS_ASESORIA
-                # ),
+                "user_has_access": Permisos.check_permissions(
+                    self.request.user, "asesoria"
+                ),
             },
             {
                 "title": "Planificación",
                 "url": "planificacion",
                 "image": "img/modules/planificacion.png",
-                "user_has_access": self.request.user.is_superuser,
-                # or any(
-                #     self.request.user.has_perm(perm)
-                #     for perm in self.PERMISSIONS_ASESORIA
-                # ),
+                "user_has_access": Permisos.check_permissions(
+                    self.request.user, "asesoria"
+                ),
             },
             {
                 "title": "Protección y Seguridad Integral",
                 "url": "seguridad",
                 "image": "img/modules/seguridad.png",
-                "user_has_access": self.request.user.is_superuser,
-                # or any(
-                #     self.request.user.has_perm(perm)
-                #     for perm in self.PERMISSIONS_ASESORIA
-                # ),
+                "user_has_access": Permisos.check_permissions(
+                    self.request.user, "asesoria"
+                ),
             },
             {
                 "title": "Biblioteca de Manuales",
                 "url": "bibliotecas",
                 "image": "img/modules/biblioteca.png",
-                "user_has_access": self.request.user.is_superuser,
-                # or any(
-                #     self.request.user.has_perm(perm)
-                #     for perm in self.PERMISSIONS_ASESORIA
-                # ),
+                "user_has_access": Permisos.check_permissions(
+                    self.request.user, "asesoria"
+                ),
             },
             {
                 "title": "Operaciones",
                 "url": "operaciones",
                 "image": "img/modules/ambulancia.png",
-                "user_has_access": self.request.user.is_superuser,
-                # or any(
-                #     self.request.user.has_perm(perm)
-                #     for perm in self.PERMISSIONS_ASESORIA
-                # ),
+                "user_has_access": Permisos.check_permissions(
+                    self.request.user, "asesoria"
+                ),
             },
             {
                 "title": "Tecnología Comunicación e Información",
                 "url": "tecnologia",
                 "image": "img/modules/tecnologia.png",
-                "user_has_access": self.request.user.is_superuser,
-                # or any(
-                #     self.request.user.has_perm(perm)
-                #     for perm in self.PERMISSIONS_ASESORIA
-                # ),
+                "user_has_access": Permisos.check_permissions(
+                    self.request.user, "asesoria"
+                ),
             },
             {
                 "title": "Gestion Comunicacional",
                 "url": "gc_info",
                 "image": "img/modules/GC_icon.png",
-                "user_has_access": self.request.user.is_superuser,
-                # or any(
-                #     self.request.user.has_perm(perm)
-                #     for perm in self.PERMISSIONS_ASESORIA
-                # ),
+                "user_has_access": Permisos.check_permissions(
+                    self.request.user, "asesoria"
+                ),
             },
         ]
         return context
--- apps/helpers/CheckPermisosMixin.py
+++ apps/helpers/CheckPermisosMixin.py
@@ -1,6 +1,8 @@
 from django.contrib import messages
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
+from django.contrib.auth.models import User
+from typing import Literal
 
 
 class CheckPermisosMixin(object):
@@ -26,3 +28,195 @@ class CheckPermisosMixin(object):
 
         messages.error(request, "No tienes permisos para realizar esta acción.")
         return redirect(self.get_url_redirect())
+
+
+AppNames = Literal["asesoria", "administracion"]
+
+
+class Permisos:
+    PERMISSIONS_ASESORIA = (
+        "asesoria.agregar_denuncia",
+        "asesoria.editar_denuncia",
+        "asesoria.eliminar_denuncia",
+        "asesoria.exel_denuncia",
+        "asesoria.listar_denuncia",
+        "asesoria.ver_denuncia",
+        "asesoria.agregar_registro_filmico",
+        "asesoria.editar_registro_filmico",
+        "asesoria.eliminar_registro_filmico",
+        "asesoria.exel_registro_filmico",
+        "asesoria.listar_registro_filmico",
+        "asesoria.ver_registro_filmico",
+    )
+
+    PERMISSIONS_GESTION_HUMANA = (
+        # CARGO
+        "rrhh.agregar_cargo",
+        "rrhh.editar_cargo",
+        "rrhh.eliminar_cargo",
+        "rrhh.exel_cargo",
+        "rrhh.listar_cargo",
+        "rrhh.ver_cargo",
+        # CONTRATO
+        "rrhh.agregar_contrato",
+        "rrhh.editar_contrato",
+        "rrhh.eliminar_contrato",
+        "rrhh.exel_contrato",
+        "rrhh.listar_contrato",
+        "rrhh.ver_contrato",
+        # DOTACION
+        "rrhh.agregar_dotacion",
+        "rrhh.editar_dotacion",
+        "rrhh.eliminar_dotacion",
+        "rrhh.exel_dotacion",
+        "rrhh.listar_dotacion",
+        "rrhh.ver_dotacion",
+        # EDUCACION
+        "rrhh.agregar_educacion",
+        "rrhh.editar_educacion",
+        "rrhh.eliminar_educacion",
+        "rrhh.exel_educacion",
+        "rrhh.listar_educacion",
+        "rrhh.ver_educacion",
+        # EMPLEAD
+        "rrhh.agregar_empleado",
+        "rrhh.editar_empleado",
+        "rrhh.eliminar_empleado",
+        "rrhh.exel_empleado",
+        "rrhh.listar_empleado",
+        "rrhh.ver_empleado",
+        # FAMILIARES
+        "rrhh.agregar_familiar",
+        "rrhh.eliminar_familiar",
+        "rrhh.exel_familiar",
+        "rrhh.listar_familiares",
+        "rrhh.modificar_familiar",
+        "rrhh.ver_familiar",
+        # TIPO_SUELDO
+        "rrhh.agregar_tipo_sueldo",
+        "rrhh.eliminar_tipo_sueldo",
+        "rrhh.exel_tipo_sueldo",
+        "rrhh.listar_tipo_sueldos",
+        "rrhh.modificar_tipo_sueldo",
+        "rrhh.ver_tipo_sueldo",
+        # SUELDO
+        "rrhh.agregar_sueldo",
+        "rrhh.eliminar_sueldo",
+        "rrhh.exel_sueldo",
+        "rrhh.listar_sueldos",
+        "rrhh.modificar_sueldo",
+        "rrhh.ver_sueldo",
+    )
+
+    PERMISSIONS_ADMINISTRACION = (
+        "administracion.agregar_tipo_articulo",
+        "administracion.editar_tipo_articulo",
+        "administracion.eliminar_tipo_articulo",
+        "administracion.agregar_articulo",
+        "administracion.editar_articulo",
+        "administracion.eliminar_articulo",
+        "administracion.listar_articulo",
+        "administracion.agregar_asignacion",
+        "administracion.editar_asignacion",
+        "administracion.eliminar_asignacion",
+        "administracion.listar_asignacion",
+        "administracion.agregar_tipo_averia",
+        "administracion.editar_tipo_averia",
+        "administracion.eliminar_tipo_averia",
+        "administracion.agregar_averia",
+        "administracion.editar_averia",
+        "administracion.eliminar_averia",
+        "administracion.listar_averia",
+        "administracion.agregar_compra",
+        "administracion.editar_compra",
+        "administracion.eliminar_compra",
+        "administracion.listar_compra",
+        "administracion.agregar_departamento",
+        "administracion.editar_departamento",
+        "administracion.eliminar_departamento",
+        "administracion.listar_departamento",
+        "administracion.agregar_sede",
+        "administracion.editar_sede",
+        "administracion.eliminar_sede",
+        "administracion.listar_sede",
+    )
+
+    PERMISSIONS_URI = (
+        "potencia.agregar_uri",
+        "potencia.editar_uri",
+        "potencia.eliminar_uri",
+        "potencia.listar_uri",
+        "potencia.ver_uri",
+    )
+
+    PERMISSIONS_POTENCIA = (
+        # TIPO_INCIDENCIA
+        "potencia.agregar_tipo_incidencia",
+        "potencia.editar_tipo_incidencia",
+        "potencia.eliminar_tipo_incidencia",
+        "potencia.listar_tipo_incidencia",
+        "potencia.ver_tipo_incidencia",
+        # INCIDENCIA
+        "potencia.agregar_incidencia",
+        "potencia.editar_incidencia",
+        "potencia.eliminar_incidencia",
+        "potencia.listar_incidencia",
+        "potencia.ver_incidencia",
+    )
+
+    PERMISSIONS_PRESUPUESTO = (
+        # ACCION
+        "presupuesto.agregar_accion",
+        "presupuesto.editar_accion",
+        "presupuesto.eliminar_accion",
+        "presupuesto.listar_accion",
+        "presupuesto.pdf_accion",
+        "presupuesto.ver_accion",
+        # ASIGNAR_PRESUPUESTO
+        "presupuesto.agregar_asignar_presupuesto",
+        "presupuesto.editar_asignar_presupuesto",
+        "presupuesto.eliminar_asignar_presupuesto",
+        "presupuesto.listar_asignar_presupuesto",
+        "presupuesto.pdf_asignar_presupuesto",
+        "presupuesto.ver_asignar_presupuesto",
+        # CEDENTE
+        "presupuesto.agregar_cedente",
+        "presupuesto.editar_cedente",
+        "presupuesto.eliminar_cedente",
+        "presupuesto.listar_cedente",
+        "presupuesto.pdf_cedente",
+        "presupuesto.ver_cedente",
+        # PROYECTO
+        "presupuesto.agregar_proyecto",
+        "presupuesto.editar_proyecto",
+        "presupuesto.eliminar_proyecto",
+        "presupuesto.listar_proyecto",
+        "presupuesto.pdf_proyecto",
+        "presupuesto.ver_proyecto",
+        # RECEPTOR
+        "presupuesto.agregar_receptor",
+        "presupuesto.editar_receptor",
+        "presupuesto.eliminar_receptor",
+        "presupuesto.listar_receptor",
+        "presupuesto.pdf_receptor",
+        "presupuesto.ver_receptor",
+    )
+
+    permisos_apps = {
+        "asesoria": PERMISSIONS_ASESORIA,
+        "gestion_humana": PERMISSIONS_GESTION_HUMANA,
+        "administracion": PERMISSIONS_ADMINISTRACION,
+        "uri": PERMISSIONS_URI,
+        "potencia": PERMISSIONS_POTENCIA,
+        "presupuesto": PERMISSIONS_PRESUPUESTO,
+    }
+
+    @classmethod
+    def check_permissions(cls, user: User, app_name: AppNames) -> bool:
+        if not user.is_authenticated:
+            return False
+
+        if user.is_superuser:
+            return True
+
+        return any(user.has_perm(perm) for perm in cls.permisos_apps[app_name])
